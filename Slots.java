7dfe1b77cadc010f352611f1ce1e0f1864c43156
bshpetner00 <bshpetner00@stuy.edu>
2019-01-04 10:00:34 -0500

    created ceelo

--- Slots.java
+++ Slots.java
@@ -1,4 +1,17 @@
 public class Slots {
 	public int slot1,slot2,slot3;
-	
+	public Slots() {
+		slot1 = ;
+		slot2 = ;
+		slot3 = ;
+	}
+	public int calculate() {
+		if (slot1 == slot2 && slot2 == slot3) {
+			return slot1;
+		}
+		
+	}
+	public void set(int val) {
+
+	}
 }
