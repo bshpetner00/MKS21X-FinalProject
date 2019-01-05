98ee02698a98cb85981f947270271a831001258a
alexanderzou <azou00@stuy.edu>
2019-01-05 17:50:42 -0500

    some slots stuff

--- Slots.java
+++ Slots.java
@@ -1,17 +1,21 @@
 public class Slots {
-	public int slot1,slot2,slot3;
-	public Slots() {
-		slot1 = ;
-		slot2 = ;
-		slot3 = ;
-	}
-	public int calculate() {
-		if (slot1 == slot2 && slot2 == slot3) {
-			return slot1;
-		}
-		
-	}
-	public void set(int val) {
-
-	}
+  public int slot1, slot2, slot3;
+  public Slots() {
+    slot1 = (Math.random() * 10) % 6;
+    slot2 = (Math.random() * 10) % 6;
+    slot3 = (Math.random() * 10) % 6;
+  }
+  public int calculate() {
+    if (slot1 == slot2 && slot2 == slot3) {
+      return slot1;
+    }
+    else {
+      return 0;
+    }
+  }
+  public void spin(int val) {
+    slot1 = (Math.random() * 10) % 6;
+    slot2 = (Math.random() * 10) % 6;
+    slot3 = (Math.random() * 10) % 6;
+  }
 }
