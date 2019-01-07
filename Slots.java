459f005ba2fad58fb1298b90c65d9fe47a2e1c8b
azou00 <azou00stuy.edu>
2019-01-07 10:01:29 -0500

    tie things together

--- Slots.java
+++ Slots.java
@@ -1,21 +1,22 @@
-public class Slots {
+public class Slots extends Game{
   public int slot1, slot2, slot3;
   public Slots() {
-    slot1 = (Math.random() * 10) % 6;
-    slot2 = (Math.random() * 10) % 6;
-    slot3 = (Math.random() * 10) % 6;
+    posX = 15;posY = 15;
+    slot1 = (Math.random() * 10) % 5 + 1;
+    slot2 = (Math.random() * 10) % 5 + 1;
+    slot3 = (Math.random() * 10) % 5 + 1;
   }
-  public int calculate() {
+  public void calculate() {
     if (slot1 == slot2 && slot2 == slot3) {
-      return slot1;
+      winnings = amountBet * slot1;
     }
     else {
-      return 0;
+      winnings = 0;
     }
   }
   public void spin(int val) {
-    slot1 = (Math.random() * 10) % 6;
-    slot2 = (Math.random() * 10) % 6;
-    slot3 = (Math.random() * 10) % 6;
+    slot1 = (Math.random() * 10) % 5 + 1;
+    slot2 = (Math.random() * 10) % 5 + 1;
+    slot3 = (Math.random() * 10) % 5 + 1;
   }
 }
