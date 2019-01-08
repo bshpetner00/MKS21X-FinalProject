e6908f469992af83f67ba1f4481cbf35f5537a62
azou00 <azou00stuy.edu>
2019-01-08 10:16:44 -0500

    more fix

--- Game.java
+++ Game.java
@@ -2,7 +2,8 @@ public class Game {
   public int amountBet;
   public int posX, posY;
   public int winnings = 0;
-  
+
+  public Game() {};
   public Game(int x, int y) {
     posX = x;
     posY = y;
