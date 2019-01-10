929e6b69977ced36b2f2a5fa70b2e7ab370dab4c
azou00 <azou00stuy.edu>
2019-01-10 09:51:45 -0500

    pre-merge

--- Game.java
+++ Game.java
@@ -2,12 +2,9 @@ public class Game {
   public int amountBet;
   public int posX, posY;
   public int winnings = 0;
-  
+
   public Game(int x, int y) {
     posX = x;
     posY = y;
-  }
-  public void bet(int v) {
-    amountBet = v;
   }
 }
