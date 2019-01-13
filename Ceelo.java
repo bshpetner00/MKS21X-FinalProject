e24aa077be7299858f98aeb4405283be723b639f
Ben Shpetner <bshpetner00@stuy.edu>
2019-01-13 18:07:24 -0500

    finished designing ceelo

--- Ceelo.java
+++ Ceelo.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Ceelo {
 	public ArrayList<Integer> playerDice,dealerDice; //arrays of 3 dice each
-	public int playerScore,dealderScore;
+	public int playerScore,dealerScore;
 	public boolean setMatch; //true if win, false if loss
 	public Ceelo() {
 		playerDice = new ArrayList<Integer>();
@@ -10,40 +10,92 @@ public class Ceelo {
 	}
 	public void roll(ArrayList<Integer> dice) {
 		for (int i = 0; i < 3; i++) {
-			dice.add((int) (Math.random() * 10) % 6 + 1);
+			dice.add((int) (Math.random() * 10) % 6 + 1); //number between 1 & 6
 		}
-		System.out.println(dice.toString() + '\n');
+		System.out.println(dice.toString());
 	}
-	public int calcScore(ArrayList<Integer> dice, int b) {
+	public void calcScore(ArrayList<Integer> dice, int b) {
 		int score = 0;
-		if (dice.contains(4) && dice.contains(5) && dice.contains(6)) {
+		if (dice.contains(4) && dice.contains(5) && dice.contains(6)) { //win condition of ceelo
 			score = 100;
 		}
-		else if (dice.get(0) == dice.get(1) && dice.get(1) == dice.get(2)) {
-			score = dice.get(0) * 3;
+		else if (dice.get(0) == dice.get(1) && dice.get(1) == dice.get(2)) { //triples
+			score = dice.get(0) * 7;
 		}
-		else if (dice.get(0) == dice.get(1) && dice.get(1) != dice.get(2)) {
+		else if (dice.get(0) == dice.get(1) && dice.get(1) != dice.get(2)) { //normal hand
 			score = dice.get(2);
 		}
-		else if (dice.get(0) == dice.get(2) && dice.get(1) != dice.get(2)) {
+		else if (dice.get(0) == dice.get(2) && dice.get(1) != dice.get(2)) { //normal hand
 			score = dice.get(1);
 		}
-		else if (dice.get(0) != dice.get(1) && dice.get(1) == dice.get(2)) {
+		else if (dice.get(0) != dice.get(1) && dice.get(1) == dice.get(2)) { //normal hand
 			score = dice.get(0);
 		}
-		else if (dice.contains(1) && dice.contains(2) && dice.contains(3)) {
+		else if (dice.contains(1) && dice.contains(2) && dice.contains(3)) { //normal hand
 			score = -100;
 		}
-		if (b == 0) {
+		if (b == 0) { //b is a binary that chooses whether to set the player's or dealer's score
 			this.playerScore = score;
+			System.out.println("Your score for this roll is " + score);
 		}
 		else if (b == 1) {
-			this.dealderScore = score;
+			this.dealerScore = score;
+			System.out.println("The dealer's score for this roll is " + score);
+		}
+		if (score == 0 && b == 0) {
+			(this.playerDice).clear();
+			this.roll(this.playerDice);
+			this.calcScore(this.playerDice,0);
+		} 
+		else if (score == 0 && b == 1) {
+			(this.dealerDice).clear();
+			this.roll(this.dealerDice);
+			this.calcScore(this.dealerDice,1);
+		}
+	}
+	public void setMatch(int pscore, int dscore) {
+		if (dscore == 0) {
+			if (pscore == 100) {
+				this.setMatch = true;
+			} 
+			else if (pscore == -100) {
+				this.setMatch = false;
+			}
+		}
+		else {
+			if (dscore == 100) {
+				this.setMatch = false;
+			}
+			else if (dscore == -100) {
+				this.setMatch = true;
+			}
+			if (pscore > dscore) {
+				this.setMatch = true;
+			}
+			if (pscore < dscore) {
+				this.setMatch = false;
+			}
+			if (pscore == dscore) {
+				(this.playerDice).clear();
+				(this.dealerDice).clear();
+				this.roll(this.playerDice);
+				this.calcScore(this.playerDice,0);
+				this.roll(this.dealerDice);
+				this.calcScore(this.dealerDice,1);
+				this.setMatch(this.playerScore,this.dealerScore);
+			}	
+		}
+		if (pscore != dscore && pscore != 0 && dscore != 0) {
+			System.out.println("You win this round =" + this.setMatch);	
 		}
 	}
 	public static void main(String[] args) {
 		Ceelo game = new Ceelo();
 		game.roll(game.playerDice);
+		game.calcScore(game.playerDice,0);
+		game.setMatch(game.playerScore,game.dealerScore);
 		game.roll(game.dealerDice);
+		game.calcScore(game.dealerDice,1);
+		game.setMatch(game.playerScore,game.dealerScore);
 	}
 }
