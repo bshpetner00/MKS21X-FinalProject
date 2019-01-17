0afbb42e3a2a6cef14642032e75b4f670bf94dd2
alexanderzou <azou00@stuy.edu>
2019-01-16 19:55:44 -0500

    Rules for Blackjack:

--- Casino.java
+++ Casino.java
@@ -99,56 +99,80 @@ public class Casino {
       }
 
       if (x == 10 & y == 15) {
-        int bet = 5; boolean endG = false;
+        int bet = 5; boolean endG = false; boolean rule = false;
         screen.clear();
         while (true) {
           KeyStroke gKey = screen.pollInput();
-          putString(1, 0, screen, "Money Left: $" + p.currency);
-          putString(1, 1, screen, "Current Bet: $" + bet);
-          putString(1, 3, screen, "Your Cards: " + bj.hand);
-          if (endG) {
-            putString(1, 8, screen, "Dealer's Total: " + bj.dTotal);
-            putString(1, 5, screen, "Dealer's Cards: " + bj.dHand);
-          }
-          else {
-            putString(1, 5, screen, "Dealer's Cards: X, " + bj.dHand.subList(0, bj.dHand.size() - 1));
-          }
-          putString(1, 7, screen, "Your Total: " + bj.total);
-          putString(1, 15, screen, "Press Up or Down to bet higher or lower");
-          putString(1, 16, screen, "Press Tab to hit, press Enter to play");
-          putString(1, 17, screen, "Press Enter after a game to reset the game");
-          if (gKey != null) {
-            if (gKey.getKeyType() == KeyType.Escape) {
-              x++; screen.clear();
-              break;}
-            else if (gKey.getKeyType() == KeyType.Tab) {
-                bj.pDraw();
-            }
-            else if (gKey.getKeyType() == KeyType.Enter) {
-              if (!endG) {
-                if (p.bet(bet, bj) == -1) {
-                  putString(1, 10, screen, "INVALID BET");
-                }
-                else {
-                  boolean play = true;
-                  while (play) {
-                    if (bj.dTotal >= 21) {play = false;}
-                    if (bj.dTotal < 17) {bj.dDraw();}
-                    else {play = false;}
-                  }
-                  bj.calculate();
-                  p.set(p.currency + bj.winnings);
-                  endG = true;
-                }
-            }
-              else {
-                bj = new Blackjack(10, 15);
-                endG = false;
+          if (rule) {
+            putString(1, 1, screen, "---RULES FOR BLACKJACK---");
+    	    putString(1, 3, screen, "- Place your bet to give to the dealer.");
+    	    putString(1, 4, screen, "- Decide whether or not you want to 'hit' - draw a card.");
+    	    putString(1, 5, screen, "- Your goal is to get higher than the dealer OR a total of 21.");
+    	    putString(1, 6, screen, "- You draw all your cards first until you are satisfied with your hand.");
+    	    putString(1, 7, screen, "- You get double your bet back when you win.");
+    	    putString(1, 10, screen, "Press Home to go back.");
+    	    if (gKey != null) {
+              if (gKey.getKeyType() == KeyType.Escape) {
+                x++; screen.clear();
+                break;}
+              else if (gKey.getKeyType() == KeyType.Home) {
+                rule = false; screen.clear();
               }
             }
-            else if (gKey.getKeyType() == KeyType.ArrowUp) {bet = bet + 5;}
-            else if (gKey.getKeyType() == KeyType.ArrowDown && bet - 5 > 0) {bet = bet - 5;}
-            screen.clear();
+          }
+          else {
+            putString(1, 0, screen, "Money Left: $" + p.currency);
+            putString(1, 1, screen, "Current Bet: $" + bet);
+            putString(1, 3, screen, "Your Cards: " + bj.hand);
+            if (endG) {
+              putString(1, 8, screen, "Dealer's Total: " + bj.dTotal);
+              putString(1, 5, screen, "Dealer's Cards: " + bj.dHand);
+            }
+            else {
+              putString(1, 5, screen, "Dealer's Cards: X, " + bj.dHand.subList(0, bj.dHand.size() - 1));
+            }
+            putString(1, 7, screen, "Your Total: " + bj.total);
+            putString(1, 15, screen, "Press Up or Down to bet higher or lower");
+            putString(1, 16, screen, "Press Tab to hit, press Enter to play");
+            putString(1, 17, screen, "Press Enter after a game to reset the game");
+            putString(1, 18, screen, "Press Home to view rules");
+            putString(1, 19, screen, "Press Escape to leave the game");
+            if (gKey != null) {
+              if (gKey.getKeyType() == KeyType.Escape) {
+                x++; screen.clear();
+                break;}
+              else if (gKey.getKeyType() == KeyType.Home) {
+                rule = true;
+              }
+              else if (gKey.getKeyType() == KeyType.Tab) {
+                  bj.pDraw();
+              }
+              else if (gKey.getKeyType() == KeyType.Enter) {
+                if (!endG) {
+                  if (p.bet(bet, bj) == -1) {
+                    putString(1, 10, screen, "INVALID BET");
+                  }
+                  else {
+                    boolean play = true;
+                    while (play) {
+                      if (bj.dTotal >= 21) {play = false;}
+                      if (bj.dTotal < 17) {bj.dDraw();}
+                      else {play = false;}
+                    }
+                    bj.calculate();
+                    p.set(p.currency + bj.winnings);
+                    endG = true;
+                  }
+              }
+                else {
+                  bj = new Blackjack(10, 15);
+                  endG = false;
+                }
+              }
+              else if (gKey.getKeyType() == KeyType.ArrowUp) {bet = bet + 5;}
+              else if (gKey.getKeyType() == KeyType.ArrowDown && bet - 5 > 0) {bet = bet - 5;}
+              screen.clear();
+            }
           }
           screen.doResizeIfNecessary();
           screen.refresh();
